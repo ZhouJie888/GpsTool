ceaf7dc90350ebb0f1918794418108fba9450cd9
ABear110 <1447754423@qq.com>
2018-08-14 10:05:42 +0800

    Signed-off-by: ABear110 <1447754423@qq.com>

---
+++ test2/app/src/main/java/entity/entity.java
@@ -0,0 +1,11 @@
+package entity;
+
+/**
+ * Created by ZHOUJIEDE on 2018/7/8.
+ */
+
+public class entity {
+    private String city;
+    private String weather;
+    private String temperature;
+}
